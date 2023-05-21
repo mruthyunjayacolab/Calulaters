5b9e3fb8b9f1e96b6109a48ef52606d1debcf3d9
mruthyunjayacolab <mruthyunjayacolab@gmail.com>
2023-05-21 14:15:07 +0530

    initial commmit

---
+++ dom.js
@@ -0,0 +1,15 @@
+console.log("Sulochana")
+console.log(document.title);
+console.log(document.body);
+console.log(document.getElementById('title'));
+console.log(document.getElementById('para'));
+
+document.title='HI  shanmukappa'
+
+let h1elmt= document.getElementById('para')
+
+////we can use the to vales to  update the vales....
+//1
+h1elmt.textContent=' i love paruu'
+//2
+h1elmt.innerText=' Values  updateded....'
---
+++ strings.js
@@ -0,0 +1,28 @@
+// let str=[1,2,3,4,5,6]
+
+// let res=str.slice(0,5)//[][ 1, 2, 3, 4, 5 ]
+// console.log(res);
+
+//let res=str.slice(-1,5)//[]
+// console.log(res);
+
+let str='mruhtyunjay'
+// let res=str.slice(0,5)//mruht
+// let res=str.substr(0,5)//mruht
+// console.log(res);
+// let res=str.slice(4,-1)//tyunja
+let res=str.substr(4,1)//empty
+// let res=str.substring(4,-1)//mruh
+/ console.log(res);
+// let res=str.slice(-3)//jay
+// let res=str.substr(-3)//jay
+
+// let res=str.slice(1,4)//ruh    
+// let res=str.substr(1,4)//ruh    
+// let res=str.substring(1,4)//ruh  
+
+
+// let res=str.slice(1,-4)//ruhtyu   
+// let res=str.slice(4,1)// empyt
+
+console.log(res);
